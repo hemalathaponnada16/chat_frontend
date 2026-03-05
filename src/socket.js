6d8c0e74b2772cf181b0806b9b21a8ac88c439ec
hema-1604 <hema@gmail.com>
2026-03-05 19:44:45 +0530

    updated socket

--- src/socket.js
+++ src/socket.js
@@ -10,6 +10,6 @@ import { io } from "socket.io-client";
 const SOCKET_URL = import.meta.env.VITE_API_URL;
 
 export const socket = io(SOCKET_URL, {
-  autoConnect: false,
+  
   withCredentials: true
 });
